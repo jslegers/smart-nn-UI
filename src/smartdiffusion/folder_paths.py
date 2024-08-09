04435b50f81459c715c85fe95c2b33d213b9529c
John Slegers <slegersjohn@gmail.com>
2024-08-09 20:22:37 +0200

    test
    test

--- src/smartdiffusion/folder_paths.py
+++ src/smartdiffusion/folder_paths.py
@@ -44,6 +44,7 @@ def create_directory(directory):
         except:
             logging.error(f"Failed to create directory {directory}")
             return None
+    return directory
 
 
 def set_base_directory(path = None):
@@ -101,9 +102,9 @@ def set_models_directory(path = None):
 
 
 
+set_base_directory()
 set_models_directory()
 set_custom_nodes_directory()
-set_base_directory()
 
 def set_output_directory(output_dir: str) -> None:
     global output_directory
@@ -243,7 +244,7 @@ def get_full_path(folder_name: str, filename: str) -> str | None:
     for x in folders[0]:
         full_path = os.path.join(x, filename)
         if os.path.isfile(full_path):
-            return create_directory(full_path)
+            return full_path
         elif os.path.islink(full_path):
             logging.warning("WARNING path {} exists but doesn't link anywhere, skipping.".format(full_path))
 
