9dad487c7f105afa92f061cce1bbaee719e83a96
John Slegers <slegersjohn@gmail.com>
2024-08-09 18:10:23 +0200

    test
    test

--- src/smartdiffusion/text_encoders/hydit.py
+++ src/smartdiffusion/text_encoders/hydit.py
@@ -2,7 +2,7 @@ from smartdiffusion import sd1_clip
 from transformers import BertTokenizer
 from smartdiffusion.text_encoders.spiece_tokenizer import SPieceTokenizer
 from smartdiffusion.text_encoders.bert import BertModel
-from smartdiffusion.text_encoders.t5
+import smartdiffusion.text_encoders.t5
 import os
 import torch
 
