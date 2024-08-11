fef05fc6e9c85155f0436cf70cc57d7e1e7808ec
John Slegers <slegersjohn@gmail.com>
2024-08-11 15:03:25 +0200

    test
    test

--- src/smartdiffusion/extra_nodes/nodes_stable_cascade.py
+++ src/smartdiffusion/extra_nodes/nodes_stable_cascade.py
@@ -17,8 +17,8 @@
 """
 
 import torch
-import smartdiffusion.config import MAX_RESOLUTION
-import smartdiffusion import utils
+from smartdiffusion.config import MAX_RESOLUTION
+from smartdiffusion import utils
 
 
 class StableCascade_EmptyLatentImage:
@@ -27,12 +27,24 @@ class StableCascade_EmptyLatentImage:
 
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": {
-            "width": ("INT", {"default": 1024, "min": 256, "max": MAX_RESOLUTION, "step": 8}),
-            "height": ("INT", {"default": 1024, "min": 256, "max": MAX_RESOLUTION, "step": 8}),
-            "compression": ("INT", {"default": 42, "min": 4, "max": 128, "step": 1}),
-            "batch_size": ("INT", {"default": 1, "min": 1, "max": 4096})
-        }}
+        return {
+            "required": {
+                "width": (
+                    "INT",
+                    {"default": 1024, "min": 256, "max": MAX_RESOLUTION, "step": 8},
+                ),
+                "height": (
+                    "INT",
+                    {"default": 1024, "min": 256, "max": MAX_RESOLUTION, "step": 8},
+                ),
+                "compression": (
+                    "INT",
+                    {"default": 42, "min": 4, "max": 128, "step": 1},
+                ),
+                "batch_size": ("INT", {"default": 1, "min": 1, "max": 4096}),
+            }
+        }
+
     RETURN_TYPES = ("LATENT", "LATENT")
     RETURN_NAMES = ("stage_c", "stage_b")
     FUNCTION = "generate"
@@ -40,13 +52,12 @@ class StableCascade_EmptyLatentImage:
     CATEGORY = "latent/stable_cascade"
 
     def generate(self, width, height, compression, batch_size=1):
-        c_latent = torch.zeros([batch_size, 16, height // compression, width // compression])
+        c_latent = torch.zeros(
+            [batch_size, 16, height // compression, width // compression]
+        )
         b_latent = torch.zeros([batch_size, 4, height // 4, width // 4])
-        return ({
-            "samples": c_latent,
-        }, {
-            "samples": b_latent,
-        })
+        return ({"samples": c_latent}, {"samples": b_latent})
+
 
 class StableCascade_StageC_VAEEncode:
     def __init__(self, device="cpu"):
@@ -54,11 +65,17 @@ class StableCascade_StageC_VAEEncode:
 
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": {
-            "image": ("IMAGE",),
-            "vae": ("VAE", ),
-            "compression": ("INT", {"default": 42, "min": 4, "max": 128, "step": 1}),
-        }}
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "vae": ("VAE",),
+                "compression": (
+                    "INT",
+                    {"default": 42, "min": 4, "max": 128, "step": 1},
+                ),
+            }
+        }
+
     RETURN_TYPES = ("LATENT", "LATENT")
     RETURN_NAMES = ("stage_c", "stage_b")
     FUNCTION = "generate"
@@ -71,22 +88,22 @@ class StableCascade_StageC_VAEEncode:
         out_width = (width // compression) * vae.downscale_ratio
         out_height = (height // compression) * vae.downscale_ratio
 
-        s = utils.common_upscale(image.movedim(-1,1), out_width, out_height, "bicubic", "center").movedim(1,-1)
+        s = utils.common_upscale(
+            image.movedim(-1, 1), out_width, out_height, "bicubic", "center"
+        ).movedim(1, -1)
 
-        c_latent = vae.encode(s[:,:,:,:3])
-        b_latent = torch.zeros([c_latent.shape[0], 4, (height // 8) * 2, (width // 8) * 2])
-        return ({
-            "samples": c_latent,
-        }, {
-            "samples": b_latent,
-        })
+        c_latent = vae.encode(s[:, :, :, :3])
+        b_latent = torch.zeros(
+            [c_latent.shape[0], 4, (height // 8) * 2, (width // 8) * 2]
+        )
+        return ({"samples": c_latent}, {"samples": b_latent})
+
 
 class StableCascade_StageB_Conditioning:
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": { "conditioning": ("CONDITIONING",),
-                              "stage_c": ("LATENT",),
-                             }}
+        return {"required": {"conditioning": ("CONDITIONING",), "stage_c": ("LATENT",)}}
+
     RETURN_TYPES = ("CONDITIONING",)
 
     FUNCTION = "set_prior"
@@ -100,7 +117,8 @@ class StableCascade_StageB_Conditioning:
             d['stable_cascade_prior'] = stage_c['samples']
             n = [t[0], d]
             c.append(n)
-        return (c, )
+        return (c,)
+
 
 class StableCascade_SuperResolutionControlnet:
     def __init__(self, device="cpu"):
@@ -108,10 +126,8 @@ class StableCascade_SuperResolutionControlnet:
 
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": {
-            "image": ("IMAGE",),
-            "vae": ("VAE", ),
-        }}
+        return {"required": {"image": ("IMAGE",), "vae": ("VAE",)}}
+
     RETURN_TYPES = ("IMAGE", "LATENT", "LATENT")
     RETURN_NAMES = ("controlnet_input", "stage_c", "stage_b")
     FUNCTION = "generate"
@@ -122,15 +138,12 @@ class StableCascade_SuperResolutionControlnet:
         width = image.shape[-2]
         height = image.shape[-3]
         batch_size = image.shape[0]
-        controlnet_input = vae.encode(image[:,:,:,:3]).movedim(1, -1)
+        controlnet_input = vae.encode(image[:, :, :, :3]).movedim(1, -1)
 
         c_latent = torch.zeros([batch_size, 16, height // 16, width // 16])
         b_latent = torch.zeros([batch_size, 4, height // 2, width // 2])
-        return (controlnet_input, {
-            "samples": c_latent,
-        }, {
-            "samples": b_latent,
-        })
+        return (controlnet_input, {"samples": c_latent}, {"samples": b_latent})
+
 
 NODE_CLASS_MAPPINGS = {
     "StableCascade_EmptyLatentImage": StableCascade_EmptyLatentImage,
