e8868c96a802fe9f65564c670e94c3e9dcce6fce
John Slegers <slegersjohn@gmail.com>
2024-08-13 05:03:44 +0200

    test
    test

--- src/smartdiffusion/__init__.py
+++ src/smartdiffusion/__init__.py
@@ -1,2 +1,11 @@
-from smartdiffusion.load import autoload
-autoload()
+import sys
+import os
+
+base_path = os.path.dirname(os.path.realpath(__file__))
+src_path = os.path.dirname(base_path)
+
+sys.path.extend([
+     os.path.join(src_path, "ComfyUI"),
+])
+
+from comfy.utils import load_torch_file
