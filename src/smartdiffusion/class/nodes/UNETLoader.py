40574fa7f5bcadc39af239f9a387194d650d43e1
John Slegers <slegersjohn@gmail.com>
2024-08-13 07:50:01 +0200

    Revert "test"
    This reverts commit c24859b2baf4762731c6e383e2e40b2984566b7b.

--- src/smartdiffusion/class/nodes/UNETLoader.py
+++ src/smartdiffusion/class/nodes/UNETLoader.py
@@ -1,5 +1,5 @@
 from torch import float8_e4m3fn, float8_e5m2
-from smartdiffusion.sd import load_unet
+from smartdiffusion.sd import load_diffusion_model
 from smartdiffusion.folder_paths import get_full_path, get_filename_list
 
 
@@ -19,8 +19,12 @@ class UNETLoader:
     CATEGORY = "advanced/loaders"
 
     def load_unet(self, unet_name, weight_dtype, path="unet"):
+        model_options = {}
         if weight_dtype == "fp8_e4m3fn":
-            dtype = float8_e4m3fn
+            model_options["dtype"] = float8_e4m3fn
         elif weight_dtype == "fp8_e5m2":
-            dtype = float8_e5m2
-        return (load_unet(get_full_path(path, unet_name), dtype=dtype),)
+            model_options["dtype"] = float8_e5m2
+
+        unet_path = get_full_path(path, unet_name)
+        model = load_diffusion_model(unet_path, model_options=model_options)
+        return (model,)
