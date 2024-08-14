2ed3e2065c8eacaec4aff5f78ba2556134d7c86e
John Slegers <slegersjohn@gmail.com>
2024-08-14 17:26:35 +0200

    test
    test

--- src/smartdiffusion/__init__.py
+++ src/smartdiffusion/__init__.py
@@ -1,7 +1,7 @@
 from smartdiffusion import nodes, load
-nodes.init_builtin_nodes("latent_preview", no_parent = True)
-nodes.init_builtin_nodes("node_helpers", no_parent = True)
+nodes.init_builtin_nodes("latent_preview.py", no_parent = True)
+nodes.init_builtin_nodes("node_helpers.py", no_parent = True)
 nodes.init_builtin_nodes("torchsde", no_parent = True)
-nodes.init_builtin_nodes("folder_paths", no_parent = True)
-nodes.init_builtin_nodes("nodes", no_parent = True)
+nodes.init_builtin_nodes("folder_paths.py", no_parent = True)
+nodes.init_builtin_nodes("nodes.py", no_parent = True)
 load.add_to_env("SMARTDIFFUSION_PATH")
