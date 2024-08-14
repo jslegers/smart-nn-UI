78b24a5f4fa4ad19a4214d0a41935bb005209169
John Slegers <slegersjohn@gmail.com>
2024-08-14 20:24:59 +0200

    test
    test

--- src/smartdiffusion/__init__.py
+++ src/smartdiffusion/__init__.py
@@ -1,6 +1,7 @@
 import sys
-from . import _, node
-setattr(_, "comfy_extras", node.extra_nodes)
+from . import _
+from .node import extra_nodes
+setattr(_, "comfy_extras", extra_nodes)
 sys.modules['smartdiffusion'] = _
 _.nodes.init_builtin_nodes("node", "nodes")
 _.nodes.init_builtin_nodes("node", "extra_nodes")
