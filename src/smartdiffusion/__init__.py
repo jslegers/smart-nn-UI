0aa577cb8d99c2e51ec6518948d2cf0474431846
John Slegers <slegersjohn@gmail.com>
2024-08-14 17:43:19 +0200

    rztest
    test

--- src/smartdiffusion/__init__.py
+++ src/smartdiffusion/__init__.py
@@ -1,7 +1,8 @@
 from smartdiffusion import nodes, load
-nodes.load_custom_node("latent_preview", no_parent = True)
-nodes.load_custom_node("node_helpers", no_parent = True)
+nodes.load_custom_node("trampoline", no_parent = True)
 nodes.load_custom_node("torchsde", no_parent = True)
+nodes.load_custom_node("latent_preview", no_parent = True)
 nodes.load_custom_node("folder_paths", no_parent = True)
+nodes.load_custom_node("node_helpers", no_parent = True)
 nodes.load_custom_node("nodes", no_parent = True)
 load.add_to_env("SMARTDIFFUSION_PATH")
