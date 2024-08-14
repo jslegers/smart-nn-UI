34024915f13077263ea02dcba57a6c892dd073ec
John Slegers <slegersjohn@gmail.com>
2024-08-14 21:56:51 +0200

    test
    test

--- src/smartdiffusion/__init__.py
+++ src/smartdiffusion/__init__.py
@@ -2,7 +2,7 @@ import sys
 import os
 from . import _
 import nodes as _nodes
-from node import extra_nodes
+from .node import extra_nodes
 sys.modules["comfy_extras"] = extra_nodes
 sys.modules[__name__] = _
 current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -12,6 +12,6 @@ for file in nodes:
     if(not node.startswith('__')):
         module_name = _nodes.get_module_name(file)
         print(module_name)
-        setattr(sys.modules[__name__], module_name, _.load.module("node", module_name))
+        setattr(sys.modules[__name__], module_name, _.load.module(".node", module_name))
 _nodes.init_builtin_nodes("node", "nodes")
 _nodes.init_builtin_nodes("node", "extra_nodes")
