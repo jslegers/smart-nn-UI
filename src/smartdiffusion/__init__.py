5d077eaad3604a4b8fe42782ba14383c35ebff5d
John Slegers <slegersjohn@gmail.com>
2024-08-14 15:15:42 +0200

    test
    test

--- src/smartdiffusion/__init__.py
+++ src/smartdiffusion/__init__.py
@@ -1,5 +1,2 @@
-import smartdiffusion
-import sys
-sys.modules["comfy"] = smartdiffusion
-sys.modules["comfy_extras"] = smartdiffusion
-smartdiffusion.load.add_to_env("SMARTDIFFUSION_PATH")
+from smartdiffusion.load import add_to_env
+add_to_env("SMARTDIFFUSION_PATH")
