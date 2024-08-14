f0d8524849531b5cc9b7b5121186fb6cdd856ee5
John Slegers <slegersjohn@gmail.com>
2024-08-14 21:22:27 +0200

    test
    test

--- src/smartdiffusion/__init__.py
+++ src/smartdiffusion/__init__.py
@@ -5,7 +5,7 @@ from .node import nodes, extra_nodes
 sys.modules["comfy_extras"] = extra_nodes
 sys.modules['smartdiffusion'] = _
 for node in dir(nodes):
-    if(not node.startswith('__'))
+    if(not node.startswith('__')):
         sys.modules['smartdiffusion.'+ node] = _.load.module("nodes", node)
 nodes.init_builtin_nodes("node", "nodes")
 nodes.init_builtin_nodes("node", "extra_nodes")
